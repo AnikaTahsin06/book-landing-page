5bbae0061ac554cbf49548083fec4d0fd4cc0229
AnikaTahsin06 <bushratahsin653@gmail.com>
2024-05-01 01:18:48 +0600

    completed the wrapper section

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,6 @@
- 
+function toggleMenu(displayState) {
+    const nav = document.getElementById('header-navigation');
+    const closeIco = document.getElementById('close-ico');
+    nav.style.display = displayState;
+    closeIco.style.display = displayState;
+}
